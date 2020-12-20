3ed5baa2698af6eac387282cd83a69e5433ccf8c
Stephan <github@athome.zone>
2020-12-20 20:46:48 +0100

    Sepidre Keymaps

---
+++ keyboards/kyria/keymaps/SepiDre/defenitions.c
@@ -0,0 +1,13 @@
+enum layers {
+    _NEO2 = 0,
+    _SHOOT,
+    _NUMBER,
+    _NUMLEFT,
+    _SYMBOLS,
+    _ADJUST
+};
+
+enum custom_keycodes {
+    ALT_TAB = SAFE_RANGE,
+    BACK_TAB = SAFE_RANGE
+};
